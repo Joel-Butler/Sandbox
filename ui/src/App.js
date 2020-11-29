12ba6720eca886c216e547961bbd2c954d78b8fc
Joel Butler <joel@jhbutler.info>
2020-11-29 09:14:16 -0500

    Basic UI with updated icons, events. Still needs some work on notification store.

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,4 +1,6 @@
 import menuIcon from './img/menu.svg';
+import homeIcon from './img/Home_Icon.svg';
+import appIcon from './img/Apps_Icon.svg';
 import {slide as Menu} from 'react-burger-menu';
 import ReactNotification from 'react-notifications-component'
 import { store as NotifyStore} from 'react-notifications-component';
@@ -7,12 +9,51 @@ import './App.css';
 import 'react-notifications-component/dist/theme.css'
 
 function App() {
+  
+  function MenuItem(props) {
+    function menuNotify(e, name){
+      NotifyStore.addNotification({
+        title: "Menu Notification!",
+        message: name + " was clicked",
+        type: "success",
+        insert: "top",
+        container: "top-right",
+        animationIn: ["animate__animated", "animate__fadeIn"],
+        animationOut: ["animate__animated", "animate__fadeOut"],
+        dismiss: {
+          duration: 5000,
+          onScreen: true
+        }
+      });
+    }
+      return <li className="menu-item" onClick={e=> menuNotify(e, props.Name)}>
+      <img className="menu-item-icon" src= {props.Icon} alt={props.IconAlt}></img>{props.Name}</li>
+
+  }
+
+  function testNotify() {
+    NotifyStore.addNotification({
+      title: "Test!",
+      message: "This is a test notification.",
+      type: "success",
+      insert: "top",
+      container: "top-right",
+      animationIn: ["animate__animated", "animate__fadeIn"],
+      animationOut: ["animate__animated", "animate__fadeOut"],
+      dismiss: {
+        duration: 5000,
+        onScreen: true
+      }
+    });
+  }
+
   return (
     <div className="App">
     <ReactNotification />
      <Menu width={ '30%' } isOpen ={ false }>
-        <a id="item1" className="menu-item" href="/">Item1</a>
-        <a id="item2" className="menu-item" href="/">Item2</a>
+       <ul>
+        <MenuItem Name="Home" Icon={homeIcon} IconAlt="House" />
+        <MenuItem Name="Apps" Icon={appIcon} IconAlt="Apps Tiles 3x3" />        </ul>
       </Menu>
       <div className="canvas">
         <header className="App-header">
@@ -31,20 +72,7 @@ function App() {
   );
 }
 
-function testNotify() {
-  NotifyStore.addNotification({
-    title: "Test!",
-    message: "This is a test notification.",
-    type: "success",
-    insert: "top",
-    container: "top-right",
-    animationIn: ["animate__animated", "animate__fadeIn"],
-    animationOut: ["animate__animated", "animate__fadeOut"],
-    dismiss: {
-      duration: 5000,
-      onScreen: true
-    }
-  });
-}
+
+
 
 export default App;
---
+++ ui/src/components/NotifyRes.js
@@ -0,0 +1,6 @@
+class notifyResources {
+    
+    
+}
+
+export default notifyResources;
